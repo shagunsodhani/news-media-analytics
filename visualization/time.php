66e98f76a1c260775e98948ab038b604ba526d7b
Shagun <sshagunsodhani@gmail.com>
2014-10-11 01:47:01 +0530

    Added initial version for time based display

---
+++ visualization/time.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html> <!--<![endif]-->
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/time.css">
+        <title>The Wealth & Health of Nations</title>
+    </head>
+    <body>
+        <!--[if lt IE 9]>
+        <div class="unsupported-browser">
+            This website does not fully support your browser.  Please get a
+            better browser (Firefox or <a href="/chrome/">Chrome</a>, or if you
+            must use Internet Explorer, make it version 9 or greater).
+        </div>
+        <![endif]-->
+
+<h1>The Wealth & Health of Nations</h1>
+
+<p id="chart"></p>
+<script src="js/jquery-1.10.2.min.js"></script> 
+<script src="js/d3.v3.min.js"></script>
+<script src="js/time.js"></script>
