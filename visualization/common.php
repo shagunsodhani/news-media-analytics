3bd25bd01d95d36340dec59f5fdf00e5e9135a6d
Shagun <sshagunsodhani@gmail.com>
2014-10-11 00:50:13 +0530

    changed default endpoint for db.php

--- visualization/common.php
+++ visualization/common.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL & ~E_NOTICE);
 require_once 'markdown/Markdown.inc.php';
 use \Michelf\Markdown;
 
-$dataset    = 'default';
+$dataset    = 'news';
 $dataset_qs = '';
 if (isset($_GET['dataset'])) {
     if (!preg_match('@[^a-z0-9-_ ]@i', $_GET['dataset'])) {
