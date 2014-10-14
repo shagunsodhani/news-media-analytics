41b76590c6d7276bc2499b0688b8998a7b9b6508
Shagun <sshagunsodhani@gmail.com>
2014-10-15 02:37:58 +0530

    updated scripts

--- visualization/search.php
+++ visualization/search.php
@@ -27,23 +27,20 @@
             </div>
 
             <div class="jumbotron">
-                <form role="search">
+
+
+                <form role="search" >
                     <div class="form-group">
                         <input type="text" class="form-control" placeholder="Search">
                     </div>
                 </form>
+
+
             </div>
 
             <div class="row marketing">
                 <div class="col-lg-12">
-                    <h4>Subheading</h4>
-                    <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-                    <h4>Subheading</h4>
-                    <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-                    <h4>Subheading</h4>
-                    <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                    
                 </div>
             </div>
         </div>
--- visualization/solr.php
+++ visualization/solr.php
@@ -4,6 +4,8 @@
     ini_set('display_errors',1);
     ini_set('html_errors', 1);
 
+    // $query = $_GET["search"];
+    // $selector  = $_GET["from"];
 
     function search($query) 
     {
@@ -11,7 +13,7 @@
                                    
         $url = "http://192.168.111.190:8983/solr/news/select?q=";
         $url = $url."headline:".$query;
-        $url = $url."&sort=startdate+desc&start=0&rows=20&fl=headline,url&wt=json&indent=true&start=0&rows=100&group=true&group.field=headline";
+        $url = $url."&sort=startdate+desc&start=0&rows=20&fl=headline,url&wt=json&indent=true&group=true&group.field=headline";
         $json = file_get_contents($url);
         $res = json_decode($json,true);
         foreach ($res['grouped']['headline']['groups'] as $i)
@@ -39,6 +41,55 @@
         echo "</div></div>";
     }
 
-    search("obama");
+    function stat_1($query) 
+    {
+        // &fl=headline&wt=json&indent=true
+        header('Content-type: text/plain');
+        $url = "http://192.168.111.190:8983/solr/news/select?q=";
+        $url = $url."headline:".$query;
+        $url = $url."&start=0&rows=150&fq=url%3Ahttp*&fl=id&wt=json&indent=true&group=true&group.field=sourceid";
+        $json = file_get_contents($url);
+        // print_r ($json);
+        $precise = json_decode($json,true);
+        $res = "";
+        foreach ($precise['grouped']['sourceid']['groups'] as $i)
+        {
+            // print_r ($i);
+            if(array_key_exists ('groupValue' , $i ) )
+            {
+                $groupValue = $i['groupValue'];
+                if(array_key_exists ('numFound' , $i['doclist'] ) )
+                {
+                    $numFound = $i['doclist']['numFound'];
+                    $res=$res.strval($groupValue)."-valid,".strval($numFound)."\n";
+                }
+            }
+        }
+        $url = "http://192.168.111.190:8983/solr/news/select?q=";
+        $url = $url."headline:".$query;
+        $url = $url."&start=0&rows=150&-fq=url%3Ahttp*&fl=id&wt=json&indent=true&group=true&group.field=sourceid";
+        echo $url;
+        $json = file_get_contents($url);
+        // print_r ($json);
+        $precise = json_decode($json,true);
+        $res = "";
+        foreach ($precise['grouped']['sourceid']['groups'] as $i)
+        {
+            // print_r ($i);
+            if(array_key_exists ('groupValue' , $i ) )
+            {
+                $groupValue = $i['groupValue'];
+                if(array_key_exists ('numFound' , $i['doclist'] ) )
+                {
+                    $numFound = $i['doclist']['numFound'];
+                    $res=$res.strval($groupValue)."-invalid,".strval($numFound)."\n";
+                }
+            }
+        }
+        echo $res;
+    }
+
+    stat_1("obama");
+    
 
 ?>
